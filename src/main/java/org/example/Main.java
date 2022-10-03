6bf580128d17e7332dd81cd3e7de8f0e92005f25
JustynaBolek <justyna.bolek@gmail.com>
2022-10-03 18:59:40 +0200

    added 3 cases

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,18 @@ public class Main {
         System.out.println("Please, enter your surname: ");
         String surname= sc.next();
         Account user=new Account(name, surname);
-
+        System.out.println("1.User details\n 2.Deposit money\n 3.Withdraw money\n");
+        int select= sc.nextInt();
+        switch (select){
+            case 1:
+                System.out.println("User details");
+                break;
+            case 2:
+                System.out.println("Deposit money");
+                break;
+            case 3:
+                System.out.println("Withdraw money");
+                break;
+        }
     }
 }
