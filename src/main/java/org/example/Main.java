ab67a5e95942554ab4cb74203a812e600f56eadc
JustynaBolek <justyna.bolek@gmail.com>
2022-10-03 20:41:14 +0200

    added option loop do while

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,35 +11,39 @@ public class Main {
         String name=sc.next();
         System.out.println("Please, enter your surname: ");
         String surname= sc.next();
+        int select;
         Account user=new Account(name, surname);
-        System.out.println("1.User details\n 2.Deposit money\n 3.Withdraw money\n");
-        int select= sc.nextInt();
-        switch (select){
-            case 1:
-                System.out.println("User details");
-                System.out.println(user.details());
-                break;
-            case 2:
-                System.out.println("Deposit money");
-                System.out.println("How much money do you want to deposit?");
-                int deposit= sc.nextInt();
-                user.deposit(deposit);
-                System.out.println("Yor balance is " + user.getBalance());
-                break;
-            case 3:
-                System.out.println("Withdraw money");
-                System.out.println("Hou much money do you want to withdraw?");
-                int withdraw= sc.nextInt();
-                if (user.getBalance()>=withdraw){
-                    user.withdraw(withdraw);
-                    System.out.println("Your current balance is " + user.getBalance());
-                }
-                else {
-                    System.out.println("We can't do this operation because you account balance is under withdraw value");
-                }
+        do {
+            System.out.println("1.User details\n 2.Deposit money\n 3.Withdraw money\n 4.Exit program");
+            select= sc.nextInt();
+            switch (select) {
+                case 1:
+                    System.out.println("User details");
+                    System.out.println(user.details());
+                    break;
+                case 2:
+                    System.out.println("Deposit money");
+                    System.out.println("How much money do you want to deposit?");
+                    int deposit = sc.nextInt();
+                    user.deposit(deposit);
+                    System.out.println("Yor balance is " + user.getBalance());
+                    break;
+                case 3:
+                    System.out.println("Withdraw money");
+                    System.out.println("Hou much money do you want to withdraw?");
+                    int withdraw = sc.nextInt();
+                    if (user.getBalance() >= withdraw) {
+                        user.withdraw(withdraw);
+                        System.out.println("Your current balance is " + user.getBalance());
+                    } else {
+                        System.out.println("We can't do this operation because you account balance is under withdraw value");
+                    }
 
-                break;
+                    break;
+                case 4:
+                    System.out.println("Exit program");
+            }
         }
-
+        while (select!=4);
         }
     }
